2b1ed2545e4a0d8cec953ebf4bb92e6543249a0a
James Cox <jamesncox@yahoo.com>
2019-07-30 18:06:21 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,7 +10,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    if @author = Author.save
+    if @author.save
       redirect_to author_path(@author)
     else
       render :new
